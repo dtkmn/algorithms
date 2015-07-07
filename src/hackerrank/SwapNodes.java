ceb3789ee901a069fcd29f73b00bb8cf72a6733f
Daniel Tse <datse@ebay.com>
2015-07-07 15:34:36 +1000

    HackerRank - Swap Nodes Practice

--- src/hackerrank/SwapNodes.java
+++ src/hackerrank/SwapNodes.java
@@ -22,16 +22,29 @@ public class SwapNodes {
     }
 
     static int height(Node root) {
-        if(root == null) return 0;
-        height(root.left);
-        height(root.right);
-        return 1 + height(root.left);
+        if (root == null) return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+
+    static void printInOrderWithSwap(Node root, int depth, final int swapDepth) {
+        if(root == null) return;
+        printInOrderWithSwap(root.left, depth + 1, swapDepth);
+        if(depth == swapDepth) {
+            Node temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+//            System.out.println("SWAP at node : " + root.data + ", with current depth " + depth + " : " + swapDepth);
+        }
+        printInOrderWithSwap(root.right, depth + 1, swapDepth);
     }
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
+
+        if(N <= 0) return;
+
         // Setup root node and make it as current
         Node root = new Node();
         root.data = 1;
@@ -41,8 +54,6 @@ public class SwapNodes {
 
         Node[] arrayNodes = new Node[N];
         arrayNodes[0] = root;
-
-        int count = 1;
 
         for(int i = 0; i<N; i++) {
             // Read a and b value in same row
@@ -67,32 +78,19 @@ public class SwapNodes {
 
         }
 
-
-
-        // Get the height of tree? (optional?)
-
+        // Get height of tree
+        int height = height(root);
 
         // Swap start
         int swaps = in.nextInt();
         for(int i = 0; i<swaps; i++) {
             int depth = in.nextInt();
-
-            Node current = root;
-            Node right = root;
-            for(int j = 0; j<depth; j++) {
-                current = current.left;
+            for(int j = depth; j < height; j+=depth) {
+//                System.out.println("in depth: " + j);
+                printInOrderWithSwap(root, 1, j);
             }
-            // should be in correct depth and start to swap
-            Node temp = current.left;
-            current.left = current.right;
-            current.right = temp;
-
+            printInOrder(root);
+            System.out.println();
         }
-        // Swap end
-
-        // Print inOrder tree
-        printInOrder(root);
-        System.out.println();
-        System.out.println("Tree height: " + height(root));
     }
 }
