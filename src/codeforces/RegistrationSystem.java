ae3d41c8cd6d18e7e9130096334de2f3788c2233
Daniel Tse <datse@ebay.com>
2015-08-17 15:55:16 +1000

    Codeforces problems: Registration System

--- src/codeforces/RegistrationSystem.java
+++ src/codeforces/RegistrationSystem.java
@@ -1,28 +1,29 @@
 package codeforces;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by dtkmn on 17/08/2015.
  */
 public class RegistrationSystem {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        String db = "";
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        Map<String, Integer> test = new HashMap<>();
         for(int i=0; i<N; i++) {
-            String name = in.next();
-            if(db.indexOf(":" + name + ":") == -1) {
-                db += ":" + name + ":";
+            String name = in.readLine();
+            if(!test.containsKey(name)) {
+                test.put(name, 0);
                 System.out.println("OK");
             } else {
-                int count = 1;
-                while(db.indexOf(":" + name + count + ":") != -1) {
-                    count++;
-                }
-                db += ":" + name + count + ":";
-                System.out.println(name + count);
+                int count = test.get(name);
+                test.put(name, ++count);
+                System.out.println(name+count);
             }
         }
     }
