e906d64418ea1b7933d5011d20a686da3bd0f0a1
Daniel Tse <datse@ebay.com>
2015-08-18 14:39:48 +1000

    Leetcode problems: Maximum Depth of Binary Tree

---
+++ src/leetcode/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+/**
+ * Created by datse on 18/08/2015.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class MaximumDepthOfBinaryTree {
+
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+}
