99f9ead93caa6f35623a4511f6c5bed99729cb45
Daniel Tse <danieltse@gmail.com>
2015-08-24 00:47:23 +1000

    Code in progress

---
+++ src/codeforces/CutRibbon.java
@@ -0,0 +1,34 @@
+package codeforces;
+
+import java.util.Scanner;
+
+/**
+ * Created by dtkmn on 23/08/2015.
+ */
+public class CutRibbon {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int total = in.nextInt();
+        int[] abc = new int[3];
+        abc[0] = in.nextInt();
+        abc[1] = in.nextInt();
+        abc[2] = in.nextInt();
+        int[] max = new int[total+1];
+        max[0] = 0;
+
+        for(int i=1; i<=total; i++) {
+            for(int j=0; j<abc.length; j++) {
+
+                if(abc[j]<=i && max[i-abc[j]]+1 > max[i]) {
+                    max[i] = max[i-abc[j]] + 1;
+                }
+
+            }
+        }
+
+        System.out.println(max[total]);
+
+    }
+
+}
