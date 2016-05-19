b45c4bb5f289dcc235378e08f2175bbaf6b52c9a
Daniel Tse <danieltse@gmail.com>
2016-05-19 13:50:01 +1000

    Elephant

---
+++ src/codeforces/Elephant.java
@@ -0,0 +1,19 @@
+package codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// http://codeforces.com/contest/617/problem/A
+public class Elephant {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        System.out.println((n+4)/5);
+    }
+
+}
