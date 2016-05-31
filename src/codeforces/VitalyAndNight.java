e17bac313c4e98a47fe2ec0a1b8bf036483dd03a
Daniel Tse <danieltse@gmail.com>
2016-05-31 11:47:18 +1000

    Codeforces: 595A Vitaly and Night

---
+++ src/codeforces/VitalyAndNight.java
@@ -0,0 +1,30 @@
+package codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// http://codeforces.com/problemset/problem/595/A
+public class VitalyAndNight {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int count = 0;
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<m; j++) {
+                if(Integer.parseInt(st.nextToken()) == 1 | Integer.parseInt(st.nextToken()) == 1) {
+                    count++;
+                }
+            }
+
+        }
+        System.out.println(count);
+    }
+
+}
