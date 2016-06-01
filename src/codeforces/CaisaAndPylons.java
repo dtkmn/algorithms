097ccd4a921b4fda1bc65e0cd13c43b801048610
Daniel Tse <danieltse@gmail.com>
2016-06-01 12:25:14 +1000

    Codeforces: 463B Caisa and Pylons

--- src/codeforces/CaisaAndPylons.java
+++ src/codeforces/CaisaAndPylons.java
@@ -1,7 +1,34 @@
 package codeforces;
 
-/**
- * Created by dtkmn on 5/08/2015.
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//http://codeforces.com/problemset/problem/463/B
 public class CaisaAndPylons {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int energy = 0, spent = 0, current = 0;
+        for(int i=0; i<n; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            if(current < x) {
+                int required = x - current;
+                if(energy < required) {
+                    spent += (required - energy);
+                    energy = 0;
+                }
+                else energy -= required;
+            } else if(current > x) {
+                energy += current - x;
+            }
+            current = x;
+        }
+        System.out.println(spent);
+    }
+
 }
