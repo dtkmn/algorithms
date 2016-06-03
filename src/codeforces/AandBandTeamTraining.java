8c591e594f1c2e909f092084476f974c562206ba
Daniel Tse <danieltse@gmail.com>
2016-06-03 10:49:59 +1000

    Codeforces: 519C A and B and Team Training

--- src/codeforces/AandBandTeamTraining.java
+++ src/codeforces/AandBandTeamTraining.java
@@ -1,7 +1,21 @@
 package codeforces;
 
-/**
- * Created by dtkmn on 6/08/2015.
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// http://codeforces.com/problemset/problem/519/C
 public class AandBandTeamTraining {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int max = (n+m)/3;
+        System.out.println(Math.min(Math.min(n, m), max));
+    }
+
 }
