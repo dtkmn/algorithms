bf28f5f17f5494897086e43d3a4e58f23b25a730
Daniel Tse <danieltse@gmail.com>
2019-11-19 00:39:11 +1100

    LeetCode problems: 709. To Lower Case

---
+++ src/leetcode/ToLowerCase.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+
+/**
+ * https://leetcode.com/problems/to-lower-case/
+ *
+ * ;-P
+ */
+public class ToLowerCase {
+
+    public static String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Result: " + toLowerCase("ZZ"));
+    }
+
+}
