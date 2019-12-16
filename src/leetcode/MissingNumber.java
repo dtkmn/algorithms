302ed592007f9c742b0aafb63fc10cb2cfeae2a3
Daniel Tse <danieltse@gmail.com>
2019-12-16 23:41:33 +1100

    LeetCode problems: 268. Missing Number

---
+++ src/leetcode/MissingNumber.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+
+/**
+ * https://leetcode.com/problems/missing-number/
+ *
+ */
+public class MissingNumber {
+
+    public static int missingNumber(int[] nums) {
+        int size = nums.length;
+        int sum = size * (size + 1) / 2;
+        for (int i = 0; i < size; i++) {
+            sum -= nums[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+            missingNumber(
+                new int[]{3,0,1}
+            )
+        );
+        System.out.println(
+            missingNumber(
+                new int[]{9,6,4,2,3,5,7,0,1}
+            )
+        );
+    }
+
+}
