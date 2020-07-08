b621d2ee37758a57b84c068c8b0dc786bc6ff3f9
Daniel Tse <danieltse@gmail.com>
2020-07-08 13:31:46 +1000

    LeetCode problems: 199. Binary Tree Right Side View

---
+++ src/leetcode/BinaryTreeRightSideView.java
@@ -0,0 +1,61 @@
+package leetcode;
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * https://leetcode.com/problems/binary-tree-right-side-view/
+ *
+ */
+public class BinaryTreeRightSideView {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static List<Integer> rightSideView(TreeNode root) {
+
+        List<Integer> res = new ArrayList<>();
+
+        if (root == null) return res;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (i == size - 1) res.add(node.val);
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
+            }
+        }
+
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(4);
+
+        System.out.println(rightSideView(root));
+    }
+
+}
