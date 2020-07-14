c9f2cd7f4c6aecf9cd71fc3f5e94ae618a67933e
Daniel Tse <danieltse@gmail.com>
2020-07-14 11:03:53 +1000

    LeetCode problems: 100. Same Tree (Iterative Approach Added)

--- src/leetcode/SameTree.java
+++ src/leetcode/SameTree.java
@@ -1,7 +1,10 @@
 package leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
- * Created by datse on 18/08/2015.
+ * https://leetcode.com/problems/same-tree/
  */
 public class SameTree {
 
@@ -9,11 +12,59 @@ public class SameTree {
 
     }
 
+    // 0ms
+    // Recursive Approach
+    // Time Complexity: O(N)
+    // Space Complexity: O(N) *Function stack
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null) return true;
-        if(p == null && q != null || p != null && q == null) return false;
-        if(p.val != q.val) return false;
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    // 0ms
+    // Iterative Approach
+    // Time Complexity: O(N)
+    // Space Complexity: O(N) i.e. 2xN on queues
+    public boolean isSameTree_bfs(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+
+        Queue<TreeNode> qp = new LinkedList<>();
+        qp.add(p);
+        Queue<TreeNode> qq = new LinkedList<>();
+        qq.add(q);
+
+        while (!qp.isEmpty() || !qq.isEmpty()) {
+            int sizep = qp.size();
+            int sizeq = qq.size();
+            if (sizep != sizeq) return false;
+
+            for (int i = 0; i < sizep; i++) {
+                TreeNode np = qp.poll();
+                TreeNode nq = qq.poll();
+                if (np.val != nq.val) return false;
+
+                if (np.left != null && nq.left != null) {
+                    qp.add(np.left);
+                    qq.add(nq.left);
+                } else if (!(np.left == null && nq.left == null)) {
+                    return false;
+                }
+
+                if (np.right != null && nq.right != null) {
+                    qp.add(np.right);
+                    qq.add(nq.right);
+                } else if (!(np.right == null && nq.right == null)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
     }
 
     public class TreeNode {
