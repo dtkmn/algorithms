6d0a14ea6458137e3d6d14ef295be1966d16bf01
Daniel Tse <danieltse@gmail.com>
2020-12-02 11:52:42 +1100

    LeetCode problems: 104. Maximum Depth of Binary Tree (Recursion/Iterative Approach)

--- src/leetcode/MaximumDepthOfBinaryTree.java
+++ src/leetcode/MaximumDepthOfBinaryTree.java
@@ -1,10 +1,13 @@
 package leetcode;
 
-/**
- * Created by datse on 18/08/2015.
- */
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
+ *
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/
+ *
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -15,9 +18,27 @@ package leetcode;
  */
 public class MaximumDepthOfBinaryTree {
 
+    // Recursion Approach
     public int maxDepth(TreeNode root) {
         if(root == null) return 0;
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    // Iterative Approach
+    public int maxDepth2(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root != null) queue.add(root);
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            depth++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+        }
+        return depth;
     }
 
     public class TreeNode {
