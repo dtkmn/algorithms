118d62520cd726814ec374380276be46057a2f5f
Daniel Tse <danieltse@gmail.com>
2023-08-14 17:54:12 +1000

    Necessary TreeNode added to run smoothly

--- src/leetcode/MaximumWidthOfBinaryTree.java
+++ src/leetcode/MaximumWidthOfBinaryTree.java
@@ -59,4 +59,16 @@ public class MaximumWidthOfBinaryTree {
 
     }
 
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
