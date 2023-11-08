7ef1e842ee9e3df60ddc45137f256a5761813541
Daniel Tse <danieltse@gmail.com>
2023-11-08 13:52:01 +1100

    LeetCode problems: 2849. Determine if a Cell Is Reachable at a Given Time

---
+++ src/leetcode/DetermineIfACellIsReachableAtAGivenTime.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class DetermineIfACellIsReachableAtAGivenTime {
+
+    public boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        if (sx == fx && sy == fy && t == 1) return false;
+        return Math.max(Math.abs(sx - fx), Math.abs(sy - fy)) <= t;
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
